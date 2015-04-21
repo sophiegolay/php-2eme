5e0e74d515b078894b3e4f0892b7995d2984edbd
sophiegolay <sophie.golay@cpnv.ch>
2015-04-21 21:13:40 +0200

    Exercice 5
    J'ai travaillé avec Lisiane mais on a pas bien compris...

---
+++ devine.php
@@ -0,0 +1,36 @@
+
+<?php
+/* ---------------------------------------------------------------------
+ *                         Ex5 chp2
+ *
+ * Auteur: SG
+ * Date: 2015 avril
+ * Description: C'est trop dur
+ * ---------------------------------------------------------------------
+ */
+ // Variables
+ // Pas de variables
+ 
+ $nombre = 0;
+ CONST = nbr_max;
+ CONST = nbr_min;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ex 5</title>
+	<h2>Jeu de devinette</h2>
+  <form name="form1" method="post" action="titre">
+    <p>Devinez le nombre entre 0 et 5<br>
+      <input type="text" name="nombre" action="resultat.php />
+    </p>
+    
+    <p>
+      <input type="submit" name="Submit" value="S'inscrire">
+      <br>
+    </p>
+  </form>
+
+  </head>
+  <body>
---
+++ resultat.php
@@ -0,0 +1,25 @@
+<?php
+/* ---------------------------------------------------------------------
+ *                         Ex5 chp2
+ *
+ * Auteur: SG
+ * Date: 2015 avril
+ * Description: C'est encore trop dur
+ * ---------------------------------------------------------------------
+ */
+ // Variables
+ // Pas de variables
+ 
+ $nombre = (int) $_POST('nombre');
+ 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ex 5</title>
+	<a href="javascript:history.back()">lien</a>
+  </form>
+
+  </head>
+  <body>
