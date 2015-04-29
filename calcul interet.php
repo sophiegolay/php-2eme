60e6573be3d22acb04a42cec5559d48849003ad2
sophiegolay <sophie.golay@cpnv.ch>
2015-04-29 18:09:53 +0200

    Chp 3
    Trop dur...

---
+++ calcul interet.php
@@ -0,0 +1,30 @@
+<?php
+/* ---------------------------------------------------------------------
+ *                        chapitre3 ex1                            
+ * Nom du fichier: calcul interet.php 
+ * Auteur: SGY
+ * Date: 29.04.15
+ * Description: calcul interet                          
+ * ---------------------------------------------------------------------
+ */
+ //variable
+ $montant_immob = 0;
+ $montant_1 =  0;
+ $montant_2 = 0;
+ $interet_1 = 0.035;
+ $interet_2 = 0.025;
+ 
+ // il faut que le montant 1 soit 20% du montant total
+ if ($montant_1 = $montant_immob * 0.20){
+ 
+ }else{
+	message+= 'Erreur'; 
+ }
+ 
+  // il faut que le montant 2 soit 50% du montant total
+ if ($montant_2 = $montant_immob * 0.50){
+ 
+ }else{
+	message+= 'Erreur'; 
+ }
+?>
---
+++ drill.php
@@ -0,0 +1,34 @@
+<?php
+/* ---------------------------------------------------------------------
+ *                        chapitre3 ex2                            
+ * Nom du fichier: drill.php 
+ * Auteur: SGY
+ * Date: 29.04.15
+ * Description: drill                          
+ * ---------------------------------------------------------------------
+ */
+ 
+ 
+ //variables
+ 
+ $multiple2 = 2;
+ $nombre = 1;
+ 
+ 
+ for (i=2; i>0; i++){
+ 
+ }
+ 
+ while (i=2; i>0; i++){
+ 
+ }
+ 
+ for (i=1; i>0; i++){
+ message =+ i + "1" ;
+ }
+ 
+ while (i=1; i>0; i++){
+ message =+ i + "1" ;
+ }
+ 
+ ?>
